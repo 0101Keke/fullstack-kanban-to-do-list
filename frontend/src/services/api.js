9eb5a94fe22259956954c174b7009c7452e47536
0101Keke <128036640+0101Keke@users.noreply.github.com>
2026-02-26 02:05:56 +0200

    Fix API base URL by removing duplicate 'http://'

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API = axios.create({ baseURL: 'http://https://fullstack-kanban-to-do-list.onrender.com/api' });
+const API = axios.create({ baseURL: 'https://fullstack-kanban-to-do-list.onrender.com/api' });
 
 // Add token to headers
 API.interceptors.request.use((req) => {
